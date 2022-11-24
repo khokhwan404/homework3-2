1b23bedbcd2eb9e091631eb6f22670c52667887e
Khokhwan Weangoukost <khim@KHiM.local>
2022-11-24 21:57:43 +0700

    submit

--- Program.cs
+++ Program.cs
@@ -17,36 +17,20 @@
 
     static void SeperateFruit(Queue<char> fruitList)
     {
-        char fruit = fruitList.Pop();
-        while (true) {
-            if (fruitList.GetLength() <= 0) {
-               break; 
-            }
-
-            if (fruit == 'L') {
-                fruitList.Push('M');
-                fruitList.Push('M');
-            } 
-            else if (fruit == 'M') {
-                fruitList.Push('S');
-                fruitList.Push('S');
-                fruitList.Push('S');
-            }  
-        }
-    }
-    
-    static void Main(string[] args) {
-        Queue<char> fruitList = new Queue<char>();
-        InputFruit(fruitList);
-
         Queue<int> processCount = new Queue<int>();
+
         for (int i = 0; i < fruitList.GetLength(); i++)
         {
             char getFruit = fruitList.Get(i);
             if (getFruit == 'L') {
+                fruitList.Push('M');
+                fruitList.Push('M');
                 processCount.Push(1);
             } 
             else if (getFruit == 'M') {
+                fruitList.Push('S');
+                fruitList.Push('S');
+                fruitList.Push('S');
                 processCount.Push(2);
             }
             else {
@@ -57,6 +41,11 @@
         for (int j = 0; j < processCount.GetLength(); j++) {
             Console.Write(processCount.Get(j));
         }
-
+    }
+    
+    static void Main(string[] args) {
+        Queue<char> fruitList = new Queue<char>();
+        InputFruit(fruitList);
+        SeperateFruit(fruitList);
     }
 }
