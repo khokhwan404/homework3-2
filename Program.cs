7e2201c599d2d49d2d6c2e75298b72eef641a75c
Khokhwan Weangoukost <khim@KHiM.local>
2022-11-24 21:40:53 +0700

    no seperate

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿class Program 
+{
+    static void InputFruit(Queue<char> fruitList)
+    {
+        char inputFruit;
+        while(true)
+        {
+            inputFruit = char.Parse(Console.ReadLine());
+            if(inputFruit != 'L' && inputFruit != 'M' && inputFruit != 'S') {
+                break;
+            }
+            else {
+                fruitList.Push(inputFruit);
+            }
+        }
+    }
+
+    static void SeperateFruit(Queue<char> fruitList)
+    {
+        char fruit = fruitList.Pop();
+        while (true) {
+            if (fruitList.GetLength() <= 0) {
+               break; 
+            }
+
+            if (fruit == 'L') {
+                fruitList.Push('M');
+                fruitList.Push('M');
+            } 
+            else if (fruit == 'M') {
+                fruitList.Push('S');
+                fruitList.Push('S');
+                fruitList.Push('S');
+            }  
+        }
+    }
+    
+    static void Main(string[] args) {
+        Queue<char> fruitList = new Queue<char>();
+        InputFruit(fruitList);
+
+        Queue<int> processCount = new Queue<int>();
+        for (int i = 0; i < fruitList.GetLength(); i++)
+        {
+            char getFruit = fruitList.Get(i);
+            if (getFruit == 'L') {
+                processCount.Push(1);
+            } 
+            else if (getFruit == 'M') {
+                processCount.Push(2);
+            }
+            else {
+                processCount.Push(3);
+            }
+        }
+
+        for (int j = 0; j < processCount.GetLength(); j++) {
+            Console.Write(processCount.Get(j));
+        }
+
+    }
+}
